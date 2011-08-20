ccdc9d8e7cc76a45f70d1e3e31db2bbacfaf6100
Adam Baranowski <alquez@Gear2ng.(none)>
2011-08-20 07:51:56 +0200

    Fixed backbutton issue

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,6 @@ BOARD_EGL_CFG := device/htc/marvel/files/egl.cfg
 
 # Recovery Portion 
 
-
-#BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
 #BOARD_HAS_LARGE_FILESYSTEM := true
